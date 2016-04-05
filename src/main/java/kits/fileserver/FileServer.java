f456300cd5d7bdedd10c2f0148c581bfc9e65219
Sandor Gabor Kocso <kocso.sandor.gabor@gmail.com>
2016-04-05 17:25:08 +0200

    Initial commit

---
+++ src/main/java/kits/fileserver/FileServer.java
@@ -0,0 +1,23 @@
+package kits.fileserver;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
+public class FileServer extends Server {
+
+	public FileServer() {
+		super(8888);
+		
+		ResourceHandler resourceHandler = new ResourceHandler();
+		resourceHandler.setDirectoriesListed(true);
+		resourceHandler.setResourceBase("public");
+        setHandler(resourceHandler);
+	}
+	
+	public void startServer() throws Exception {
+		start();
+        dumpStdErr();
+        join();
+	}
+	
+}
---
+++ src/main/java/kits/fileserver/Main.java
@@ -0,0 +1,9 @@
+package kits.fileserver;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		new FileServer().startServer();
+	}
+	
+}
