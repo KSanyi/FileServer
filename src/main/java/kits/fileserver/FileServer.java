95f7084855940c0f5d4c54e4fe259b4c8a59364c
Sanyi <kocso.sandor.gabor@gmail.com>
2016-08-04 12:09:11 +0200

    cleanup

--- src/main/java/kits/fileserver/FileServer.java
+++ src/main/java/kits/fileserver/FileServer.java
@@ -7,17 +7,17 @@ public class FileServer extends Server {
 
 	public FileServer() {
 		super(8888);
-		
+
 		ResourceHandler resourceHandler = new ResourceHandler();
 		resourceHandler.setDirectoriesListed(true);
 		resourceHandler.setResourceBase("public");
-        setHandler(resourceHandler);
+		setHandler(resourceHandler);
 	}
-	
+
 	public void startServer() throws Exception {
 		start();
-        dumpStdErr();
-        join();
+		dumpStdErr();
+		join();
 	}
-	
+
 }
